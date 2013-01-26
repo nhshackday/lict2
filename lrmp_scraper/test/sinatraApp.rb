a793fa1e1d147ee79201deb11e2be29ea25392c8
Alexander James Phillips <ajp.global@gmail.com>
2013-01-26 13:43:36 +0000

    add gems for sinatra

--- lrmp_scraper/test/sinatraApp.rb
+++ lrmp_scraper/test/sinatraApp.rb
@@ -1,18 +1,8 @@
-#require 'rubygems'
 require 'sinatra'
-#require 'sinatra/reloader'
-require 'sinatra/content_for'
-require 'sinatra/partial'
 require 'json'
 
 require 'pry'
 
-set :partial_template_engine, :erb
-enable :partial_underscores
-enable :sessions
-#set :public_folder, File.dirname(__FILE__) + '/' #serve everything
-@@test_data ||= {}
-
-get '/' do
-  "SINATRA IS READY on port: #{settings.port}"
+get '/test/webpage-spec-frames/index.html' do
+  send_file File.join(File.dirname(__FILE__), '/test/webpage-spec-frames/index.html', params[:any])
 end
