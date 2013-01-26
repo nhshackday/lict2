b1d09fb73acdb8ad1266024d4a107811d89849ca
Kristian Glass <git@doismellburning.co.uk>
2013-01-26 15:17:01 +0000

    Display Doctors

--- lict2/conflict/views.py
+++ lict2/conflict/views.py
@@ -1,1 +1,8 @@
 # Create your views here.
+from django.views.generic.list import ListView
+from .mongomodels import Doctor
+
+class DoctorListView(ListView):
+    queryset = Doctor.objects.all()
+    context_object_name = 'doctor_list'
+    template_name = "conflict/doctor_list.html"
