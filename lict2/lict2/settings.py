e8af90125dbd98d2e665ea1870f2ae913ecf2184
Kristian Glass <git@doismellburning.co.uk>
2013-01-26 14:36:24 +0000

    CSV->mongo import

--- lict2/lict2/settings.py
+++ lict2/lict2/settings.py
@@ -1,4 +1,6 @@
 # Django settings for lict2 project.
+import os
+import mongoengine
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -150,3 +152,5 @@ LOGGING = {
         },
     }
 }
+
+mongoengine.connect('xxx', host=os.environ['MONGODB_URL']) #TODO At some point, work out a less icky way of doing this...
