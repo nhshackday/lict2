8b797929f9e4793ee65e3cba1acb70996fc54765
Alexander James Phillips <ajp.global@gmail.com>
2013-01-26 15:23:58 +0000

    get status of iframe

--- lrmp_scraper/test/sinatraApp.rb
+++ lrmp_scraper/test/sinatraApp.rb
@@ -3,6 +3,16 @@ require 'json'
 
 require 'pry'
 
-get '/test/webpage-spec-frames/index.html' do
-  send_file File.join(File.dirname(__FILE__), '/test/webpage-spec-frames/index.html', params[:any])
+#  from lict2/lrmp_scraper/test   run with `ruby sinatraApp.rb -p 4000`
+
+
+#not in use, if we run into trouble navigating frames then we'll come back here to learn
+
+get '/frame2-3.html' do
+  sleep 10
+  send_file File.join(File.dirname(__FILE__), "/webpage-spec-frames/frame2-3.html")
+end
+
+get '/:file' do
+  send_file File.join(File.dirname(__FILE__), "/webpage-spec-frames/#{params[:file]}")
 end
