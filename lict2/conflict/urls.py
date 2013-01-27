e3f7d129d8da6befc80f4787c85890dde8c69363
Alexander James Phillips <ajp.global@gmail.com>
2013-01-27 13:13:38 +0000

    fix merge

--- lict2/conflict/urls.py
+++ lict2/conflict/urls.py
@@ -3,11 +3,12 @@
 from __future__ import print_function, division, absolute_import, unicode_literals
 from django.conf.urls import patterns, url
 from django.shortcuts import redirect
-from .views import DoctorListView
+from .views import DoctorListView, StudyListView
 from .views import RootRedirect
 
 urlpatterns = patterns('conflict.views',
     url(r'^doctors/$', DoctorListView.as_view()),
     url(r'^doctors/(?P<page>[0-9]+)/$', DoctorListView.as_view()),
+    url(r'^studies/$', StudyListView.as_view()),
     url(r'^$', RootRedirect.as_view()),
 )
--- lict2/conflict/views.py
+++ lict2/conflict/views.py
@@ -1,8 +1,9 @@
 # Create your views here.
 from django.views.generic.base import RedirectView
 from django.views.generic.list import ListView
+
 #from django.core.paginator import Paginator
-from .mongomodels import Doctor
+from .mongomodels import Doctor, Study
 import pdb
 
 class RootRedirect(RedirectView):
@@ -15,3 +16,8 @@ class DoctorListView(ListView):
     paginate_by = 2
     context_object_name = 'doctor_list'
     template_name = "conflict/doctor_list.html"
+
+class StudyListView(ListView):
+    queryset = Study.objects.all()
+    context_object_name = 'study_list'
+    template_name = 'conflict/study_list.html'
