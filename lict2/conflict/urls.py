fe90b86d157ddcf47f5f0e1bbd8132aa93a3625b
Alexander James Phillips <ajp.global@gmail.com>
2013-01-27 13:10:00 +0000

    add some pagination to doctors view

--- lict2/conflict/urls.py
+++ lict2/conflict/urls.py
@@ -2,8 +2,12 @@
 
 from __future__ import print_function, division, absolute_import, unicode_literals
 from django.conf.urls import patterns, url
+from django.shortcuts import redirect
 from .views import DoctorListView
+from .views import RootRedirect
 
 urlpatterns = patterns('conflict.views',
-    url(r'doctors/$', DoctorListView.as_view()),
+    url(r'^doctors/$', DoctorListView.as_view()),
+    url(r'^doctors/(?P<page>[0-9]+)/$', DoctorListView.as_view()),
+    url(r'^$', RootRedirect.as_view()),
 )
--- lict2/conflict/views.py
+++ lict2/conflict/views.py
@@ -1,8 +1,17 @@
 # Create your views here.
+from django.views.generic.base import RedirectView
 from django.views.generic.list import ListView
+#from django.core.paginator import Paginator
 from .mongomodels import Doctor
+import pdb
+
+class RootRedirect(RedirectView):
+    url = "doctors/"
+    permanent = False
 
 class DoctorListView(ListView):
     queryset = Doctor.objects.all()
+    #paginator = Paginator(queryset)
+    paginate_by = 2
     context_object_name = 'doctor_list'
     template_name = "conflict/doctor_list.html"
