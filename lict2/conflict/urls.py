3f13cdb6886ee76f90b7df5a3bcf6fa365ac648f
Kristian Glass <git@doismellburning.co.uk>
2013-01-27 15:09:47 +0000

    /doctors/interesting/

--- lict2/conflict/urls.py
+++ lict2/conflict/urls.py
@@ -3,11 +3,12 @@
 from __future__ import print_function, division, absolute_import, unicode_literals
 from django.conf.urls import patterns, url
 from django.shortcuts import redirect
-from .views import DoctorListView, StudyListView, LandingPage
+from .views import DoctorListView, StudyListView, LandingPage, InterestingDoctorListView
 from .views import RootRedirect
 
 urlpatterns = patterns('conflict.views',
     url(r'^doctors/$', DoctorListView.as_view()),
+    url(r'^doctors/interesting/$', InterestingDoctorListView.as_view()),
     url(r'^studies/$', StudyListView.as_view()),
     url(r'^$', LandingPage.as_view()),
 )
