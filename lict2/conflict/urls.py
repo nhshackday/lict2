83ecb6bc396b8e2db75e88e660e9b01a24b0be77
Kristian Glass <git@doismellburning.co.uk>
2013-01-27 12:43:58 +0000

    Studies view

--- lict2/conflict/urls.py
+++ lict2/conflict/urls.py
@@ -2,8 +2,9 @@
 
 from __future__ import print_function, division, absolute_import, unicode_literals
 from django.conf.urls import patterns, url
-from .views import DoctorListView
+from .views import DoctorListView, StudyListView
 
 urlpatterns = patterns('conflict.views',
     url(r'doctors/$', DoctorListView.as_view()),
+    url(r'studies/$', StudyListView.as_view()),
 )
--- lict2/conflict/views.py
+++ lict2/conflict/views.py
@@ -1,8 +1,13 @@
 # Create your views here.
 from django.views.generic.list import ListView
-from .mongomodels import Doctor
+from .mongomodels import Doctor, Study
 
 class DoctorListView(ListView):
     queryset = Doctor.objects.all()
     context_object_name = 'doctor_list'
     template_name = "conflict/doctor_list.html"
+
+class StudyListView(ListView):
+    queryset = Study.objects.all()
+    context_object_name = 'study_list'
+    template_name = 'conflict/study_list.html'
