3b16b487860967abd654276a50ad114e9454422a
Kristian Glass <git@doismellburning.co.uk>
2013-01-27 12:37:46 +0000

    UKCRN importer

---
+++ lict2/conflict/management/commands/import_from_ukcrn.py
@@ -0,0 +1,45 @@
+# -*- coding: UTF-8 -*-
+
+from __future__ import print_function, division, absolute_import, unicode_literals
+import logging
+from bs4 import BeautifulSoup
+from django.core.management.base import BaseCommand
+from conflict.mongomodels import Study
+
+logger = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        for file_name in args:
+            logger.info("Starting import from %s" % file_name)
+            Study.objects.delete() #TODO Sanity
+            with open(file_name, 'rb') as data_file:
+                html_doc = data_file.read()
+
+                if b"No study found" in html_doc:
+                    continue
+
+                doc = BeautifulSoup(html_doc)
+
+                # ARGH
+
+                def find_Cell_from_title(title):
+                    def find_TitleCell_by_content(content):
+                        """
+                        I'm so so sorry
+                        """
+                        cells = doc.find_all(class_="TitleCell")
+                        for cell in cells:
+                            if cell.string.strip() == content:
+                                return cell
+                    title_cell = find_TitleCell_by_content(title)
+                    tr = title_cell.parent
+                    td = tr.find(class_="Cell")
+                    return td.string
+
+                ukcrn_id = find_Cell_from_title("UKCRN ID")
+                (study, _) = Study.objects.get_or_create(ukcrn_id=ukcrn_id)
+                study.save()
+
+            logger.info("Finished import from %s" % file_name)
+        logger.info("Finished importing %s", ', '.join(args))
