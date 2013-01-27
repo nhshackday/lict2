a0c42a459dfcea7d4f6ae4c4ab0fdffe3527b46f
Kristian Glass <git@doismellburning.co.uk>
2013-01-27 13:47:16 +0000

    Massive try/except blocks because I have 0 time

--- lict2/conflict/management/commands/import_from_ukcrn.py
+++ lict2/conflict/management/commands/import_from_ukcrn.py
@@ -13,38 +13,41 @@ class Command(BaseCommand):
         for file_name in args:
             logger.info("Starting import from %s" % file_name)
             with open(file_name, 'rb') as data_file:
-                html_doc = data_file.read()
+                try:
+                    html_doc = data_file.read()
 
-                if b"No study found" in html_doc:
-                    continue
+                    if b"No study found" in html_doc:
+                        continue
 
-                doc = BeautifulSoup(html_doc)
+                    doc = BeautifulSoup(html_doc)
 
-                # ARGH
+                    # ARGH
 
-                def find_Cell_from_title(title):
-                    def find_TitleCell_by_content(content):
-                        """
-                        I'm so so sorry
-                        """
-                        cells = doc.find_all(class_="TitleCell")
-                        for cell in cells:
-                            if cell.string.strip() == content:
-                                return cell
-                    title_cell = find_TitleCell_by_content(title)
-                    tr = title_cell.parent
-                    td = tr.find(class_="Cell")
-                    if td.string:
-                        return td.string.strip()
-                    if len(td.contents) > 1:
-                        return td.contents[0].strip()
+                    def find_Cell_from_title(title):
+                        def find_TitleCell_by_content(content):
+                            """
+                            I'm so so sorry
+                            """
+                            cells = doc.find_all(class_="TitleCell")
+                            for cell in cells:
+                                if cell.string.strip() == content:
+                                    return cell
+                        title_cell = find_TitleCell_by_content(title)
+                        tr = title_cell.parent
+                        td = tr.find(class_="Cell")
+                        if td.string:
+                            return td.string.strip()
+                        if len(td.contents) > 1:
+                            return td.contents[0].strip()
 
-                ukcrn_id = int(find_Cell_from_title("UKCRN ID"))
-                (study, _) = Study.objects.get_or_create(ukcrn_id=ukcrn_id)
-                study.name = doc.find_all("p", align="justify")[0].string.strip()
-                study.funder = find_Cell_from_title("Funder(s)")
-                study.sponsor = find_Cell_from_title("Sponsor(s)")
-                study.save()
+                    ukcrn_id = int(find_Cell_from_title("UKCRN ID"))
+                    (study, _) = Study.objects.get_or_create(ukcrn_id=ukcrn_id)
+                    study.name = doc.find_all("p", align="justify")[0].string.strip()
+                    study.funder = find_Cell_from_title("Funder(s)")
+                    study.sponsor = find_Cell_from_title("Sponsor(s)")
+                    study.save()
+                except Exception as e: # I AM A VERY VERY BAD PERSON
+                    logger.warn(e)
 
             logger.info("Finished import from %s" % file_name)
         logger.info("Finished importing %s", ', '.join(args))
