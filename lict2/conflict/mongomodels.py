97674573c57a4d6a8a88b81fa59aa8c047953919
Kristian Glass <git@doismellburning.co.uk>
2013-01-27 10:22:28 +0000

    Updated Mongo models

--- lict2/conflict/mongomodels.py
+++ lict2/conflict/mongomodels.py
@@ -2,18 +2,14 @@
 
 from __future__ import print_function, division, absolute_import, unicode_literals
 from mongoengine.document import Document
-from mongoengine.fields import StringField
+from mongoengine.fields import StringField, IntField, DateTimeField
 
 class Doctor(Document):
     """
-    Ideally we'd use the LRMP list. We don't have that, but we do have a CSV subset...
-
-    Massively temporary: Last name,First name,Title,Type,Site name,Address1,Address2,Address3,Postcode,Telephone,Institution
+    #keys = ['GMC Ref Number', 'Given Names', 'Surname', 'Status', 'Prov Reg Date', 'Full Reg Date', 'Annual Fee Due Date', '']
+    3527470, Peter Robert, Ahee, Registered with a licence to practise, N/A, 10/01/91, 10/01/14,
     """
-    last_name = StringField()
-    first_name = StringField()
-    title = StringField()
-    type = StringField() #TODO Enumalike?
-    site_name = StringField()
-    # TODO Address?
-    institution = StringField() #TODO Reference
+    surname = StringField()
+    given_names = StringField()
+    gmc_reference_number = IntField()
+    registration_date = DateTimeField()
